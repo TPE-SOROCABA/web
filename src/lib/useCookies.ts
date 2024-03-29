2a9a11d0ac5759de0b1605802e2b89227e554427
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2024-03-29 14:15:13 -0300

    mudando componente sidebar para usar Drawer do material tailwind

--- src/lib/useCookies.ts
+++ src/lib/useCookies.ts
@@ -1,5 +1,23 @@
 import type { CookieSetOptions } from "universal-cookie";
 import { useCookies as useReactCookie } from "react-cookie";
+import {jwtDecode} from "jwt-decode";
+
+export interface IToken {
+  id:          string;
+  name:        string;
+  profile:     string;
+  groupId:     string;
+  designation: Designation;
+  iat:         number;
+  exp:         number;
+}
+
+export interface Designation {
+  id:         string;
+  expiration: Date;
+  name:       string;
+}
+
 
 export const useCookies = () => {
   const [cookies, setReactCookie, removeReactCookie] = useReactCookie();
@@ -16,7 +34,13 @@ export const useCookies = () => {
     removeReactCookie(name);
   };
 
-  return { set, get, erase };
+  const decodeToken = (): IToken | null => {
+    const token = get("token");
+    if (!token) return null;
+    return jwtDecode(token);
+  }
+
+  return { set, get, erase, decodeToken };
 };
 
 type Key = "token";
