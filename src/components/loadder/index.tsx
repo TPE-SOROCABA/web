a59998dbce1121fcc84b33426b58ec442f744bba
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2024-05-02 01:45:33 -0300

    centralizando loader

--- src/components/loadder/index.tsx
+++ src/components/loadder/index.tsx
@@ -2,7 +2,7 @@
 
 export const Loader = () => {
     return (
-        <div className="flex justify-center items-center mt-14">
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <div className="loader"></div>
         </div>
     );
